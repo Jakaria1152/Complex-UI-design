6febf74d72cdf4b2baf1bc1f235979aaee2368cd
Md. Jakaria Ibna Azam Khan <mdjakariaibnaazamkhan@gmail.com>
2024-04-13 23:21:16 +0600

    update this on 13-04-24

--- lib/home.dart
+++ lib/home.dart
@@ -30,7 +30,60 @@ class _HomePageState extends State<HomePage> {
             SizedBox(
               height: 20,
             ),
-            header3_home()
+            header3_home(),
+            SizedBox(
+              height: 5,
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 30),
+              height: 180,
+              width: MediaQuery.of(context).size.width,
+              child: Stack(
+                children: [
+                  Container(
+                    height: 120,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      image: DecorationImage(
+                        image: AssetImage("assets/img/background.jpg"),
+                        fit: BoxFit.fill    // fill dile image nosto na kore total height and width nibe
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 40,
+                          offset: Offset(8,10),
+                          color: color.AppColor.gredientsecond.withOpacity(0.3),
+
+
+                        ),
+                        BoxShadow(
+                          blurRadius: 20,
+                          offset: Offset(-1,-5),
+                          color: color.AppColor.gredientsecond.withOpacity(0.3),
+
+
+                        ),
+                      ]
+                    ),
+                  ),
+                  Container(
+                    height: 200,
+                    width: MediaQuery.of(context).size.width,
+margin: EdgeInsets.only(right: 200,bottom: 70),
+                    decoration: BoxDecoration(
+                      color: Colors.redAccent.withOpacity(0.4),
+                        borderRadius: BorderRadius.circular(20),
+                        image: DecorationImage(
+                            image: AssetImage("assets/img/boy run.jpg"),
+                            fit: BoxFit.fill    // fill dile image nosto na kore total height and width nibe
+                        ),
+
+                    ),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
